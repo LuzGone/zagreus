e299788f02c8a406d20304c0f019f2cadb024174
Luiz Gonzaga <luizg.academico@gmail.com>
2023-09-17 15:26:07 -0300

    Tela Win e Lose funcionando de forma base

--- app/src/main/java/br/edu/ifpb/pdm/zagreus/WinActivity.kt
+++ app/src/main/java/br/edu/ifpb/pdm/zagreus/WinActivity.kt
@@ -5,28 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.textfield.TextInputEditText
 
 class WinActivity : AppCompatActivity() {
     private lateinit var botaoWin: Button
-    private lateinit var nomeDoJogador: TextInputEditText
+    private lateinit var nomeDoJogador: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win)
 
+        this.nomeDoJogador = this.findViewById(R.id.nomeDoJogador)
         this.botaoWin = this.findViewById(R.id.botaoWin)
-        this.botaoWin.setOnClickListener({retornar()})
 
-        this.nomeDoJogador = this.findViewById(R.id.nomeJogador)
+
+        this.botaoWin.setOnClickListener({retornar()})
     }
 
     private fun retornar(){
-        //Estou enviando para a view que me chamou o nome do jogador
-        var nomeDoJogador = this.nomeDoJogador.text.toString()
-        val intent = Intent().apply {
-            putExtra("NOME_JOGADOR",nomeDoJogador)
-        }
-        setResult(RESULT_OK, intent)
         finish()
     }
 }
